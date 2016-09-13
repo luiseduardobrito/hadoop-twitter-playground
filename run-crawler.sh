11e900413bfcb6d9b2dbafd686af17dab518e5e9
Luis Eduardo Brito <luis@devnup.com>
2016-09-12 22:18:18 -0300

    Build: New gitignore and env template

--- run-crawler.sh
+++ run-crawler.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+. envs.sh
 ./crawler.py "$1" | ./mapper.py | sort -k1,1 | ./reducer.py | sort -nk2 | ./reporter.py | sort -nk2
--- run-streamer.sh
+++ run-streamer.sh
@@ -3,4 +3,5 @@
 set -e
 set -o pipefail
 
+. envs.sh
 python streamer.py $1 | ./mapper.py | sort -k1,1 | ./reducer.py | sort -nk2
